ca109513ff5b35e6fc0be16d98ec7f95bc5752e3
MMcg80 <shirokuma025@gmail.com>
2026-05-16 15:06:22 -0400

    Views Templte Lab complete

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView  # <-- Imported RedirectView
+from onlinecourse import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('onlinecourse/', include('onlinecourse.urls')),
+    path('', include('onlinecourse.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
